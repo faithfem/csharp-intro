5e5ea93713ee713f6426a620622701788d72f583
faithfem <fmugure@intracitygeeks.org>
2017-11-25 23:39:19 -0500

    updates

--- variables/variables/variables/Program.cs
+++ variables/variables/variables/Program.cs
@@ -19,7 +19,10 @@ namespace variables
             Console.WriteLine(character);
             Console.WriteLine(firstName);
             Console.WriteLine(isWorking);
-            
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            //Console.WriteLine(const float Pi = 3.14f);
+
         }
     }
 }
