29aa1c342de2946d17d7e1ffa8bf1bda1a4395d2
faithfem <fmugure@intracitygeeks.org>
2017-11-26 00:20:32 -0500

    Conversion Types

---
+++ TypeConversion/typeConversion/typeConversion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            int j = 100;
+            byte a = (byte)j;
+            Console.WriteLine(a);
+
+            var number = "1234";
+            int x = Convert.ToInt32(number);
+            Console.WriteLine(x);
+
+            try
+            {
+                var num = "700";
+                byte y = Convert.ToByte(num);
+                Console.WriteLine(y);
+            }
+            catch (Exception)
+            {
+                //throw;
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+
+        }
+    }
+}
