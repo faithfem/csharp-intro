57efe25af9c7289439d9fc26ce07e94b6d60cad5
faithfem <fmugure@intracitygeeks.org>
2017-11-25 22:15:59 -0500

    changed variables

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,7 +11,6 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Connecting to VS github extension");
         }
     }
 }
--- variables/variables/variables/Program.cs
+++ variables/variables/variables/Program.cs
@@ -7,8 +7,18 @@ namespace variables
     {
         static void Main(string[] args)
         {
-            byte number = 5;
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Faith";
+            bool isWorking = false;
             Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
             
         }
     }
