86d5b2dde4e70b11e545cb086ca9de8743e050f1
faithfem <fmugure@intracitygeeks.org>
2017-11-27 17:30:43 -0500

    Section6 Complete

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2017, 11, 27);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+            Console.WriteLine("Today is: " + today);
+
+            //Once created, dates & time are immutable, so can only be changed using ADD
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+            Console.WriteLine("Tomorrow is: " + tomorrow + " and yesterday was: " + yesterday);
+
+            //To turn time into a string
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString()); //Can take a format specifier. See line below
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+
+            //VIDEO 59: TIME SPAN
+            //CREATING TIME SPANS:
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            //Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            //ADD: Time Span is also immutable but can be modified using ADD
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(8)));
+
+            //TimeSpan to String
+            Console.WriteLine("ToString " + timeSpan.ToString());
+
+            //Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+
+
+
+
+
+        }
+    }
+}
