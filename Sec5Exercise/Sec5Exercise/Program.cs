9e476787b0d8ca51e1c76436d511f4bf7379f409
faithfem <fmugure@intracitygeeks.org>
2017-11-26 21:45:09 -0500

    Num btw 1 and 100 divisible by 3

---
+++ Sec5Exercise/Sec5Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec5Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i<=100; i++)
+            {
+                if (i%3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
