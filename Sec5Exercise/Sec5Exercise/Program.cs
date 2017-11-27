2544a803ee463992e78e6f32f11381ab6c941405
faithfem <fmugure@intracitygeeks.org>
2017-11-26 23:52:29 -0500

    Section 6 Demo Arrays

--- Sec5Exercise/Sec5Exercise/Program.cs
+++ Sec5Exercise/Sec5Exercise/Program.cs
@@ -10,14 +10,15 @@ namespace Sec5Exercise
     {
         static void Main(string[] args)
         {
-            for (var i = 0; i<=100; i++)
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
             {
-                if (i%3 == 0)
-                {
-                    Console.WriteLine(i);
-                    //Console.WriteLine(count(i));
-                }
+                if (i % 3 == 0)
+                //Console.WriteLine(i);
+                count++;
             }
+            Console.WriteLine("There are {0} numbers between 1 and 100 that are divisible by 3", count);
         }
     }
 }
+
