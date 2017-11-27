33b66e445eccbfe988210ca3b91cf944bb9bb867
faithfem <fmugure@intracitygeeks.org>
2017-11-26 19:33:34 -0500

    completed sec1-5

---
+++ DemoRefTypesandValueTypes/DemoRefTypesandValueTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRefTypesandValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("a: {0}, b: {1}", a, b); //This prints same as line below
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b)); //This prints same as line above
+
+            var arraykcpeTy = new int[3] {1, 2, 3};
+            var arraykcpeLy = arraykcpeTy;
+            arraykcpeLy[0] = 0;
+            Console.WriteLine(arraykcpeLy[1]); //[1] prints 2
+        }
+    }
+}
---
+++ Demostrings/Demostrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demostrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Faith";
+            var lastName = "efm";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+
+            var text = "Hi John\nLook into paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            var textt = @"Hi John
+Look into paths
+c:\folder5\folder6
+c:\folder7\folder8";
+            Console.WriteLine(text);
+            Console.WriteLine(textt);
+        }
+    }
+}
