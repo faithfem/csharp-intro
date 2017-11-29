c59a857a515d91920a07e0ca5af389609cd23daa
faithfem <fmugure@intracitygeeks.org>
2017-11-28 20:10:04 -0500

    Section10 Lecture78-Debugging

---
+++ Sec10-Lec78 Debugging/Sec10-Lec78 Debugging/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec10_Lec78_Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DEBUGGING
+
+            /*WILL NOT RUN ANY CODE. JUST WRITING NOTES
+            I) PUT ONE OR MORE BREAKPOINTS IN YOUR APP
+            II) PUT A BREAKPOINT BY PRESSING F9 (AND F9 AGAIN TO UNBREAK WHEN DONE)
+            III) NOW RUN APP IN DEBUG MODE BY PRESSING "F5"
+            PS: CTRL+F5 TO RUN WITHOUT DEBUT
+            IV) WHEN U RUN, IT WILL STOP AT LINE WHERE YOU PUT THE BREAKPOINT
+            V) YOU CAN STEP OVER THE CODE LINE BY LINE BY PRESSING F10
+            VI) TO STEP INTO THE LINE, PRESS F11
+            VII) HOVER OVER EACH LINE TO SEE IF RESULTS ARE AS EXPECTED
+            VIII) VERY IMPTT TOOL IN DEBUGGING: OPEN WATCH WINDOW: DEBUG > WINDOWS > WATCH > WATCH 1
+            IX) ENTER THE VARIABLES U WANT TO LOOK AT IN THIS WATCH WINDOW SO U DON'T HAVE TO HOVER ONE BY ONE
+            X) SHIFT+F11 TO STEP OUT OF A LINE
+            XI) F5 RUNS APP IN DEBUG MODE, SHIFT+F5 TO STOP THE DEBUG MODE
+
+        }
+    }
+}
