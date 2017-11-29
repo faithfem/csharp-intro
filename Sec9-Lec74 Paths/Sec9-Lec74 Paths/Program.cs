8ec17f653ea35beecd24f6e65c7d7830d8202658
faithfem <fmugure@intracitygeeks.org>
2017-11-28 19:51:52 -0500

    Section 9 complete

---
+++ Sec9-Lec74 Paths/Sec9-Lec74 Paths/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec9_Lec74_Paths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TO SHOW THE PATH OF FILES AND DIRECTORIES
+
+            var path = @"C:\Coding\Projects\CSharpFundamentals\WhileLoops\WhileLoops.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+
+        }
+    }
+}
---
+++ Sec9-Lecture73-Directory/Sec9-Lecture73-Directory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec9_Lecture73_Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DIRECTORY & DIRECTORY INFO (LIKE FILE & FILE INFO) PROVIDE STATIC & INSTANCE METHODS
+            //(I) DIRECTORY          
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            //BELOW CODE WILL SHOW ALL FILES IN THE FOLDER SHOWN
+            var files = Directory.GetFiles(@"C:\Coding\Projects\CSharpFundamentals", "*.*"); //Has 3 overloads
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            //TO RETURN ALL THE DIRECTORIES
+            var directories = Directory.GetDirectories(@"C:\Coding\Projects\CSharpFundamentals", "*.*",
+                SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            //TO SEE IF A DIRECTORY EXISTS
+            Directory.Exists("...");
+
+
+            //(II) DIRECTORY INFO
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories()
+        }
+    }
+}
