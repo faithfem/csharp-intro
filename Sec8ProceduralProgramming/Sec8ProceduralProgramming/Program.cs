14464cc063ea4e2fd83c61e10295412c48fad4ef
faithfem <fmugure@intracitygeeks.org>
2017-11-28 19:18:16 -0500

    Section 9-Demo File & File Info

---
+++ Sec8ProceduralProgramming/Sec8ProceduralProgramming/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec8ProceduralProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Section9/Section9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            // FILE COPY - PROVIDES STATIC METHODS
+            File.Copy("@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg",true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path); // To read all text in a file. Takes a path and returns a string
+
+            //FILE INFO: PROVIDES INSTANCE METHODS FIRST CREATE AN OBJECT
+
+            var fileInfo = new FileInfo(path); // Specify the path. To copy this file, see line below
+            fileInfo.CopyTo("...");
+            fileInfo.Delete(); //If you want to delete, do this, no parameters
+            if (fileInfo = new FileInfo(path);//To find if file exists and this is not a method, it's a property
+            {
+                // do something
+            }
+
+        }
+
+    }
+}
